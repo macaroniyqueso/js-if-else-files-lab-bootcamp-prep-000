3fcd80c9eeb57fe34e83caf8fc0e84e62b406387
alejandramvillagra@gmail.com Villagra <alejandramvillagra@gmail.com>
2018-03-16 03:32:44 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -3,13 +3,15 @@ var index = require("./index.js")
 if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("Expected: Susan, Received: "+index.name)
+    console.log("Expected the name to be Susan")
 }
 
 
 
-if (index.height === 70) {
+if (index.height === 74) {
     console.log("The height is correct")
 } else {
-    console.log("Expected: 70, Received: "+index.height)
+    console.log("Expected the height to be correct")
 }
+nodejstests.js
+index.js
